d6dde3c51d8ee6eaef05f97b520244ed2a2fe768
jaeman Kim <daga4242@gmail.com>
2026-01-14 08:37:02 +0900

    dollar / Green: implement Dollar.times() to pass multiplication test

--- src/main/java/com/inc/tdd_practice/Dollar.java
+++ src/main/java/com/inc/tdd_practice/Dollar.java
@@ -1,11 +1,13 @@
 package com.inc.tdd_practice;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
+        amount *= multiplier;
     }
 }
