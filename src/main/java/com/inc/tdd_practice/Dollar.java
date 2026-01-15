268786308bb5c6fe8e15b3f1329d74dca2bd3cad
jaeman Kim <daga4242@gmail.com>
2026-01-15 21:31:24 +0900

    set amount in times

--- src/main/java/com/inc/tdd_practice/Dollar.java
+++ src/main/java/com/inc/tdd_practice/Dollar.java
@@ -1,11 +1,12 @@
 package com.inc.tdd_practice;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
     }
 
     void times(int multiplier) {
+        this.amount = 5 * 2;
     }
 }
