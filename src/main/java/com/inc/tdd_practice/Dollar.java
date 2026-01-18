2e52db21200ef8a71e0a50897e12884d7706100c
jaeman Kim <daga4242@gmail.com>
2026-01-18 16:30:39 +0900

    feat: Dollar 곱셈 테스트 추가 및 가짜 구현(Fake it)으로 통과

--- src/main/java/com/inc/tdd_practice/Dollar.java
+++ src/main/java/com/inc/tdd_practice/Dollar.java
@@ -1,11 +1,13 @@
 package com.inc.tdd_practice;
 
 public class Dollar {
-    int amount = 5;
+    int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
+        this.amount = 5 * 2;
     }
 }
