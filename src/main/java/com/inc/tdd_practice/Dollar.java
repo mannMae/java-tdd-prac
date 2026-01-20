64e7af8f8aa07bca40cc6215d2ca35acee9bf280
jaeman Kim <daga4242@gmail.com>
2026-01-20 09:05:17 +0900

    MoneyTest & Dollar 스텁구현

---
+++ src/main/java/com/inc/tdd_practice/Dollar.java
@@ -0,0 +1,12 @@
+package com.inc.tdd_practice;
+
+public class Dollar {
+    int amount = 5;
+
+    Dollar(int amount) {
+    }
+
+    void times(int multiplier) {
+        this.amount = 5 * 2;
+    }
+}
