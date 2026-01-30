03ff6f53d2c27e28c1f9f59b2ba4eb08181e295a
jaeman Kim <daga4242@gmail.com>
2026-01-30 21:20:13 +0900

    Cucumber 테스트 작성

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,14 @@
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+    // 여기는 아무 내용도 적지 마세요!
+    // 설정값(Annotation)들이 알아서 큐컴버를 실행해줍니다.
+}
